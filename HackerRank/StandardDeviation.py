311065c4a2d06a1af208b99f2002a94c8bc727b4
Chinh <chduong@users.noreply.github.com>
2020-01-16 19:55:56 -0500

    StandardDeviation

---
+++ HackerRank/StandardDeviation.py
@@ -0,0 +1,26 @@
+import math
+
+f = open('data/input.txt', 'r')
+
+n = int(f.readline())
+array1 = [int(x) for x in f.readline().split()]
+
+def mean(arr1):
+    sum = 0
+    for i in array1:
+        sum = sum + i
+    mean_val = sum / n
+    return mean_val
+
+# print(mean(array1))
+
+def standardDeviation(arr1):
+    u = mean(arr1)
+    # print(u)
+    sigma_numerator = 0
+    for i in range(n):
+        sigma_numerator = sigma_numerator + (arr1[i] - u)**2
+    sigma = math.sqrt(sigma_numerator / n)
+    print(round(sigma, 1))
+
+standardDeviation(array1)
