b44685ac6328eec651ee3379809d695230be02e9
Chinh <chduong@users.noreply.github.com>
2020-01-17 18:19:19 -0500

    Puzzles, TallMenNextDoor

---
+++ Puzzles/TallMenNextDoor.py
@@ -0,0 +1,31 @@
+# Next door to me live four brothers of different heights. Their average height is 74 inches, and the difference in height amongst each of the first three men is two inches (i.e. One is 2 inches away from Two and Two is 2 inches away from Three). The difference between the third and fourth man is six inches.
+#
+# Can you tell how tall is each brother?
+
+# We know the average height:
+# mean height = 74
+#
+# Therefore:
+# x / 4 = 74
+
+# Rearranging x:
+# x = 74 * 4
+
+#We also know the difference in height between each brother:
+# brother1 = x' - 4
+# brother2 = x' - 2
+# brother3 = x'
+# brother4 = x' + 6
+
+# We can now break x into x' (x prime) parts:
+# (x' - 4) + (x' - 2) + (x') + (x' + 6) = 74 * 4
+
+# Simplifying the expression:
+# 4x' = 74 * 4
+# x' = 74
+
+# Finally the height of each brother is:
+# brother1 = 74 - 4 = 70
+# brother2 = 74 - 2 = 72
+# brother3 = 74
+# brother4 = 74 + 6 = 80
