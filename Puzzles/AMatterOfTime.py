b7c877053909cf43214fa71361fd45eb7c1371bc
Chinh <chduong@users.noreply.github.com>
2020-01-19 04:01:37 -0500

    Solutions for AMatterOfTime.py

---
+++ Puzzles/AMatterOfTime.py
@@ -0,0 +1,17 @@
+# A Matter of Time
+# Fifty minutes ago if it was four times as many minutes past three o'clock, how many minutes is it to six o'clock?
+
+# To start, the total time in minutes between 3 and 6 o'clock is 180 minutes.
+
+# We also know that from some time x before 6 o'clock, and 50 minutes before that was 4x minutes past three o'clock.
+
+# Pictorial timeline broken down into segments (+ indicates time added to go from 3 to 6 o'clock:
+# 3 o'clock | +4x | +50 minutes (to get to a time between 5 and 6 o'clock) | +x | 6 o'clock
+
+# Total time between 3 and 6 o'clock is 180 minutes.
+# From this timeline we get: 4x + 50 + x = 180 minutes
+# This simplifies to 5x = 130 minutes
+# x = 26 minutes
+
+# So the current time is 3 o'clock + 4x (where x = 26) + 50 minutes = 5:34
+# 5:34 + x minutes (where x = 26) = 6 o'clock
---
+++ Puzzles/README.md
@@ -0,0 +1,6 @@
+<h2> Puzzles to Puzzle You </h2>
+<h3> By Shakuntala Devi </h3>
+
+<main>
+<p1> My solutions for puzzles from the book.
+</main>
